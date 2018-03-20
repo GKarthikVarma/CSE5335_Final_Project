bf69b56276369df8f887b80fb1c8d987fa7b693f
91huangi <91huangi@gmail.com>
2018-03-20 18:04:28 -0500

    Updated form UI

--- header_login.php
+++ header_login.php
@@ -7,17 +7,19 @@ session_start();
 <head>
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <header>
 	<nav>
 		<div class="main-wrapper">
-		<div id="title">
+		<div class="title">
 		MavConnect
 	</div>
 		<div class="navbar">
 			<ul>
 				<li><a href="profile.php">Profile</a></li>
+				<li><a href="post_job.php">Post a Job</a></li>
 				<li><a href="search.php">Search</a></li>
 				<li><a href="index.php">Signout</a></li>
 			</ul>
