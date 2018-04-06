597fb7511adf68f552750e0f390627482fc95c9a
Karthik Varma Gadiraju <34803966+GKarthikVarma@users.noreply.github.com>
2018-04-06 02:43:24 -0500

    Update edit_profile.php

--- edit_profile.php
+++ edit_profile.php
@@ -1,86 +1,126 @@
-
 <?php
-	include_once 'header_login.php';
-	include_once 'background/dbh.php';
 
-	$sql = "SELECT * FROM students WHERE user_id=".$_SESSION['u_id'];
-	$result = mysqli_query($connection, $sql);
-	$row = mysqli_fetch_assoc($result);
-?>
+session_start();
 
 
-  <div class="main-container">
+if(isset($_POST['save_profile'])){
 
-  <div class="profile-container">
+	include_once 'dbh.php';
 
-  <form class="formA" action="background/edit_profile.php" method="post" enctype="multipart/form-data">
-    <input type="hidden" name="id" value=<?php echo("{$_SESSION['u_id']}")?>>
-    <input type="hidden" name="uid" value=<?php echo("{$_SESSION['u_uid']}")?>>
-    <label>First Name:</label><br />
-    <input type="text" name="first_name" value='<?php echo $row['user_first'] ?>'></input><br /><br />
-    <label>Last Name:</label><br />
-    <input type="text" name="last_name" value='<?php echo $row['user_last'] ?>'></input><br /><br />
-    <label>E-Mail:</label><br />
-    <input type="text" name="email" value='<?php echo $row['user_email'] ?>'></input><br /><br />
-    <label>Degree Type:</label><br />
-    <select id='degree_type' name="degree_type">
-      <option value="Associates">Associates</option>
-      <option value="Bachelors">Bachelors</option>
-      <option value="Masters">Masters</option>
-      <option value="Doctorate">Doctorate</option>
-    </select>
-    in
-    <input type="text" name="major"  value='<?php echo $row['user_degree_in'] ?>'></input>
-    <br /><br />
-    <label>Graduation:</label><br />
-    <select id='grad_semester' name="grad_semester">
-      <option value="Fall">Fall</option>
-      <option value="Spring">Spring</option>
-    </select>
-    of
-    <input type="number" name="grad_year"  value='<?php echo $row['user_graduation_year'] ?>'></input><br /><br />
-    <label>Skills (seperate by comma):</label><br />
-    <textarea name="skills"><?php echo $row['user_skills'] ?></textarea>
-    <br /><br />
-    <label>Open to Relocation</label>
-    <input type="checkbox" name="relocation"  <?php if($row['user_relocation']==1) { echo "checked"; } ?>></input><br /><br />
+	$first_edit = $_POST['first_name'];
+	$last_edit = $_POST['last_name'];
+	$user_id = $_POST['uid'];
+	$email = $_POST['email'];
+	$degree_type = $_POST['degree_type'];
+	$major = $_POST['major'];
+	$grad_sem = $_POST['grad_semester'];
+	$grad_year = $_POST['grad_year'];
+	$skills = $_POST['skills'];
+	$relocation = $_POST['relocation'];
+	$uid = $_POST['id'];
 
-    <label>Upload Photo</label><br />
-    <input type="file" name="photo"><br /><br />
 
-    <label>Upload Resume</label><br />
-    <input type="file" name="resume" accept=".pdf, .doc"><br /><br />
+	$photo = $_FILES['photo'];
+	$photoName = $_FILES['photo']['name'];
+	$photoTmpName = $_FILES['photo']['tmp_name'];
+	$photosize = $_FILES['photo']['size'];
+	$photoError = $_FILES['photo']['error'];
+	$photoType = $_FILES['photo']['type'];
+	$fileExt = explode(".", $photoName);
+	$fileactext = strtolower(end($fileExt));
 
-    <center>
-    <button type="submit" name="save_profile">Save Profile</button>
-  </center>
-		<script>
-			<?php
-				$select1=0;
-				$select2=0;
-				if($row['user_degree'] == "Bachelors") {
-					$select1=1;
-				} else if ($row['user_degree'] == "Masters") {
-					$select1=2;
-				} else if ($row['user_degree'] == "Doctorate") {
-					$select1=3;
+	$allow = array('jpg', 'jpeg', 'png');
+
+	$resume = $_FILES['resume'];
+	$resumeName = $_FILES['resume']['name'];
+	$resumeTmpName = $_FILES['resume']['tmp_name'];
+	$resumeSize = $_FILES['resume']['size'];
+	$resumeError = $_FILES['resume']['error'];
+	$resumetype = $_FILES['resume']['type'];
+
+	$resumeExt = explode(".", $resumeName);
+	$resumeacttext = strtolower(end($resumeExt));
+
+	$allow_resume = array('pdf', 'docx');
+
+
+
+	if (empty($first_edit) || empty($last_edit) || empty($degree_type) || empty($major) || empty($grad_sem) || empty($grad_year) || empty($skills)) {
+		header("Location: ../edit_profile.php?fill_all");
+		exit();
+	} else {
+		if ($relocation=='on') {
+			$relocation = 1;
+		} else {
+			$relocation = 0;
+		}
+
+		if (in_array($fileactext, $allow)){
+			if ($photoError === 0) {
+				if ($photosize < 1000000){
+					$photoNameNew = $user_id.".".$fileactext;
+
+					$photoDestination = '../uploads/'.$photoNameNew;
+					if (move_uploaded_file($photoTmpName, $photoDestination)){
+					} else {
+						header("Location: ../edit_profile.php?$photoDestination");
+						exit();
+					}
+
+				} else {
+					echo "File too big";									
 				}
+			} else {
+				echo "There was an error uploading your file";				
+			}
+		} else {
+			echo "You cannot upload files of this type";
+		}
 
-				if($row['user_graduation_semester']=="Spring") {
-					$select2=1;
+		if (in_array($resumeacttext, $allow_resume)){
+			if ($resumeError === 0) {
+				if ($resumeSize < 1000000000){
+					$resumeNewName = $user_id."_resume".".".$resumeacttext;
+
+					$resume_destination = '../uploads/'.$resumeNewName;
+					if (move_uploaded_file($resumeTmpName, $resume_destination)){
+					} else {
+						header("Location: ../edit_profile.php?$resume_destination");
+						exit();
+					}
+
+				} else {
+					echo "File too big";									
 				}
-			 ?>
+			} else {
+				echo "There was an error uploading your file";				
+			}
+		} else {
+			echo "You cannot upload files of this type";
+		}
 
-			 document.getElementById('degree_type').selectedIndex=<?php echo $select1; ?>;
-			 document.getElementById('grad_semester').selectedIndex=<?php echo $select2; ?>;
-		</script>
-  </form>
 
+		$_SESSION['u_id'] = $uid;
+		$_SESSION['u_first'] = $first_edit;
+		$_SESSION['u_last'] = $last_edit;
+		$_SESSION['u_email'] = $email;
+		$_SESSION['u_uid'] = $user_id;
+		$_SESSION['u_degree'] = $degree_type;
+		$_SESSION['u_degree_in'] = $major;
+		$_SESSION['u_graduation_semester'] = $grad_sem;
+		$_SESSION['u_graduation_year'] = $grad_year;
+		$_SESSION['u_skills'] = $skills;
+		$_SESSION['u_relocation'] = $relocation;
+		$_SESSION['u_photo'] = $photoNameNew;
+		$_SESSION['u_resume'] = $resumeNewName;
+	
+		$sql = "UPDATE students set user_first='$first_edit', user_last='$last_edit', user_degree='$degree_type', user_degree_in='$major', user_graduation_semester='$grad_sem', user_email='$email', user_graduation_year='$grad_year', user_skills='$skills', user_relocation='$relocation', user_photo='$photoNameNew', user_resume='$resumeNewName' WHERE user_id='$uid';";
+		$result = mysqli_query($connection, $sql);
+		header("Location: ../profile.php?login=success");
+		exit();	
+	}
 
-
-  </div>
-  </div>
-
-  <?php
-  	include_once 'footer.php';
-  ?>
+} else {
+	header("Location: ../edit_profile.php");
+	exit();
+}
