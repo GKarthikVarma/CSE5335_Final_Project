21a6cd7e440ad45cd1f3034a614283d0731dcd4e
91huangi <91huangi@gmail.com>
2018-04-26 11:39:42 -0500

    Added assets

---
+++ background/dbh.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbServername = "127.0.0.1:3306";
+$dbUsername = "root";
+$dbPassword = "fakTBs98";
+$dbName = "loginsystem";
+
+$connection = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+?>
