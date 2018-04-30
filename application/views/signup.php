15e3d7d7f2dc53d205d96190e9ff823a906895ec
Karthik Varma Gadiraju <34803966+GKarthikVarma@users.noreply.github.com>
2018-04-29 22:54:27 -0500

    Add files via upload

--- application/views/signup.php
+++ application/views/signup.php
@@ -8,31 +8,26 @@
 		<h2 id="register">Sign Up for MavConnect</h2>
 
 		<?php 
-	        echo form_open('signup/submit');
-                echo form_label('First : ');
+	        echo form_open('signup/submit', "class='signup-form'");
                 echo form_error('dfirst');
-                echo form_input(array('id' => 'dfirst', 'name' => 'dfirst'));
+                echo form_input(array('id' => 'dfirst', 'name' => 'dfirst', 'placeholder' => 'First Name'),"","class='text-input'" );
                 echo "<br>";
-                echo form_label('Last :');
                 echo form_error('dLast');
-                echo form_input(array('id' => 'dlast', 'name' =>'dlast'));
+                echo form_input(array('id' => 'dlast', 'name' =>'dlast', 'placeholder' => 'Last Name'),"","class='text-input'" );
                 echo "<br>";
-                echo form_label('Email :');
                 echo form_error('demail');
-                echo form_input(array('id' => 'demail', 'name' =>'demail'));
+                echo form_input(array('id' => 'demail', 'name' =>'demail','placeholder' => 'Email'),"","class='text-input'" );
                 echo "<br>";
-                echo form_label('Username :');
                 echo form_error('duid');
-                echo form_input(array('id' => 'duid', 'name' =>'duid'));
+                echo form_input(array('id' => 'duid', 'name' =>'duid', 'placeholder' => 'Username'),"","class='text-input'" );
                 echo "<br>";
-                echo form_label('Password :');
                 echo form_error('dpass');
-                echo form_password(array('id' => 'dpwd', 'name' =>'dpwd'));
+                echo form_password(array('id' => 'dpwd', 'name' =>'dpwd','placeholder' => 'password'),"","class='text-input'" );
                 echo "<br>";
                 $options = array('Student', 'Recruiter');
                 echo form_dropdown("utype", $options);
                 echo "<br>";
-                echo form_submit('submit', 'submit');
+                echo form_submit('submit', 'submit', "class='submit-button'");
                 echo form_close();
 		?>
 	</center>
